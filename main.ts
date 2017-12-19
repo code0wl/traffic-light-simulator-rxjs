0a5fe049c44f5ef06d49e977d45eb75e3d99982e
code0wl <o.lodriguez@gmail.com>
2017-12-19 22:42:34 +0100

    game engine complete

--- main.ts
+++ main.ts
@@ -1,26 +1,19 @@
 import Canvas from "./src/module/engine/canvas/canvas";
 import {Display, iResolution} from "./src/module/engine/display/display";
 import AnimationLoop from "./src/module/engine/animation/animation_engine";
-import Controls from "./src/module/engine/controls/controls";
 
 class TrafficLightSimulator {
     public canvas: Canvas;
     public cars: number = 12;
     public lights: number = 12;
     private resolution: iResolution;
-    private controls: Controls;
 
     constructor(public animationLoop: AnimationLoop) {
         this.resolution = Display();
         this.canvas = new Canvas(this.resolution.width, this.resolution.height);
-        this.controls = new Controls();
-        this.startSimulation();
-    }
 
-    private startSimulation() {
         this.animationLoop.animationEngine$
-            .takeUntil(this.controls.toggle$)
-            .subscribe(this.render);
+            .subscribe(this.render)
     }
 
     private render() {
